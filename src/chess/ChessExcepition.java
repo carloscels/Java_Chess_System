77b0df97fcc7dc28961348c19220674ca23297d5
caducels <carlos.cels.1999@gmail.com>
2022-07-31 04:14:09 -0300

    sistema de xadrez

---
+++ src/chess/ChessExcepition.java
@@ -0,0 +1,11 @@
+package chess;
+
+import boardgame.BoardExcepition;
+
+public class ChessExcepition extends BoardExcepition {
+    private static final long serialVersionUID = 1L;
+
+    public ChessExcepition(String msg) {
+        super(msg);
+    }
+}
